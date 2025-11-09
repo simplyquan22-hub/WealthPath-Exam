33b3fc08778a545acd0b8443e5aa784a71905652
Rae’Quan Colbourne <raequancol12@gmail.com>
2025-11-09 19:30:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "wealthpath-test-52045489-ca2b5",
+  "appId": "1:558535809585:web:c82ad50864900f92341310",
+  "apiKey": "AIzaSyCnewSee_6T5uHA1TUKMu_WlhCjSkbT6tU",
+  "authDomain": "wealthpath-test-52045489-ca2b5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "558535809585"
+};
